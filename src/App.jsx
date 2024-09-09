dc51712d31db94d584ce873ffd72ee2cf1891aee
priyanshyv <priyansh.yv7@gmail.com>
2024-09-09 14:29:43 +0530

    heading update and buildupdate

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import { Box } from "@chakra-ui/react";
+import { Box ,Heading} from "@chakra-ui/react";
 import CodeEditor from "./components/Codeeditor";
 
 function App() {
   return (
     <Box minH="100vh" bg="#0f0a19" color="gray.500" px={6} py={8}>
+      <Heading 
+        color="white" 
+        size="4xl" 
+        fontWeight="bold"
+        textAlign="center" 
+        mb={8} 
+        style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)" }}
+      >
+        Online code runner
+      </Heading>
       <CodeEditor />
     </Box>
   );
